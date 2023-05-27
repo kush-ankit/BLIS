147459235e489aa11e71a6bd4846730031ee3c2e
Ankit Kushwaha <er.ankitkush@gmail.com>
2023-05-27 23:32:22 +0530

    password matching

--- server/controllers/user.login.js
+++ server/controllers/user.login.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const userModel = require('../data/user.model');
+const bcrypt = require('bcrypt');
 const userLoginRouter = new express.Router();
 
 userLoginRouter.use(bodyParser.urlencoded({ extended: false }))
@@ -9,9 +11,20 @@ userLoginRouter.get('/', (req, res) => {
     res.send("create a post request to login youself");
 });
 
-userLoginRouter.post('/',(req, res) => {
+userLoginRouter.post('/', async (req, res) => {
     const {email, password} = req.body;
-    
+    const saveUser = await userModel.find({email: email}).exec();
+    if(email !== '' && password !== '') {
+        bcrypt.compare(password, saveUser[0].password).then(function(result) {
+            if(result){
+                res.send(saveUser);
+            }else{
+                res.status(400).send('Invalid email or Password');
+            }
+        });
+    }else{
+        res.status(400).send('Empty email or Password');
+    }
 });
 
 module.exports = userLoginRouter;
