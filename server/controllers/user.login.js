0aa3486f596d033406a6d88ffb5cb766a239f709
Ankit Kushwaha <er.ankitkush@gmail.com>
2023-05-28 00:19:03 +0530

    jwt added

--- server/controllers/user.login.js
+++ server/controllers/user.login.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userModel = require('../data/user.model');
 const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 const userLoginRouter = new express.Router();
+
+const secretKey = 'ajdfhudflnneuicjs';
 
 userLoginRouter.use(bodyParser.urlencoded({ extended: false }))
 userLoginRouter.use(bodyParser.json());
@@ -12,17 +15,18 @@ userLoginRouter.get('/', (req, res) => {
 });
 
 userLoginRouter.post('/', async (req, res) => {
-    const {email, password} = req.body;
-    const saveUser = await userModel.find({email: email}).exec();
-    if(email !== '' && password !== '') {
-        bcrypt.compare(password, saveUser[0].password).then(function(result) {
-            if(result){
-                res.send(saveUser);
-            }else{
+    const { email, password } = req.body;
+    const saveUser = await userModel.find({ email: email }).exec();
+    if (email !== '' && password !== '') {
+        bcrypt.compare(password, saveUser[0].password).then(function (result) {
+            if (result) {
+                const token = jwt.sign({ phone: saveUser[0].phone, email: saveUser[0].email }, secretKey);
+                res.cookie(token).send(saveUser);
+            } else {
                 res.status(400).send('Invalid email or Password');
             }
         });
-    }else{
+    } else {
         res.status(400).send('Empty email or Password');
     }
 });
